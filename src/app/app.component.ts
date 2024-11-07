3fcae74ada189a40da8efa19e3f6fb9ff27b1b93
JoseSanchezM <jose.sanchez.m@tecsup.edu.pe>
2024-11-07 13:23:39 -0500

    repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NosotrosComponent } from "./nosotros/nosotros.component";
+import { ServiciosComponent } from "./servicios/servicios.component";
+import { ContactoComponent } from "./contacto/contacto.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NosotrosComponent, ServiciosComponent, ContactoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
-import { Routes } from '@angular/router';
+/* import { Routes } from '@angular/router';
 
 export const routes: Routes = [];
+ */
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { ProductosComponent } from './productos/productos.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { MarcasComponent } from './marcas/marcas.component';
+import { ContactoComponent } from './contacto/contacto.component';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'nosotros', component: NosotrosComponent },
+  { path: 'servicios', component: ServiciosComponent },
+  { path: 'productos', component: ProductosComponent },
+  { path: 'marcas', component: MarcasComponent },
+  { path: 'contacto', component: ContactoComponent },
+];
+
+/*@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }*/
